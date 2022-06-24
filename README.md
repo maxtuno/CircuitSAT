3493865c4a92104895fb4a7d9d45c6ff22c080fe
SAT-X <oscar.riveros@peqnp.science>
2022-06-24 03:47:41 -0400

    CircuitSAT 0.0.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# Circuit SAT Assembler
+
+    pip install circuitsat
+
+# opcodes
+    not                 ; A = NOT B
+    and                 ; A = B AND C
+    or                  ; A = B OR C
+    nand                ; A = B NAND C
+    nor                 ; A = B NOR C
+    xor                 ; A = B XOR C
+    equ                 ; A = B EQU C
+    copy                ; A = B
+    new                 ; Create a new BIT on REGISTER.
+    push                ; Push REGISTER to STACK.
+    pop                 ; Pop STACK to REGISTER.
+    true                ; Set BIT on REGISTER as TRUE.
+    false               ; Set BIT on REGISTER as FALSE.
+    set ALPHANUMERIC    ; Save REGISTER at SLOT ALPHANUMERIC.
+    get ALPHANUMERIC    ; Load SLOT ALPHANUMERIC into REGISTER.
+    cur NUMBER          ; Set the current BIT to NUMBER.
+    end                 ; Mandatory at the end of file.
+
+# Note
+Work in progress!
---
+++ setup.py
@@ -0,0 +1,38 @@
+"""
+Copyright (c) 2012-2021 Oscar Riveros [https://twitter.com/maxtuno].
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+import sys
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='CircuitSAT',
+    version='0.0.0',
+    packages=['circuitsat'],
+    url='https://twitter.com/maxtuno',
+    license='MIT',
+    author='Oscar Riveros',
+    description='A Circuit SAT Assembler that emit CNF code.',
+    install_requires=['antlr4-python3-runtime'],
+    entry_points={'console_scripts': ['csat=circuitsat._csat:main']},
+)
