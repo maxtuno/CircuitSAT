4ed35192e7a55f6811e96316102a183419081b7e
Oscar Riveros <oscar.riveros@gmail.com>
2022-06-24 03:48:57 -0400

    Add files via upload

---
+++ circuitsat/CircuitSATListener.py
@@ -0,0 +1,48 @@
+# Generated from C:/Users/oscar/Downloads/Development/circuitsat\CircuitSAT.g4 by ANTLR 4.10.1
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .CircuitSATParser import CircuitSATParser
+else:
+    from CircuitSATParser import CircuitSATParser
+
+# This class defines a complete listener for a parse tree produced by CircuitSATParser.
+class CircuitSATListener(ParseTreeListener):
+
+    # Enter a parse tree produced by CircuitSATParser#circuitsat.
+    def enterCircuitsat(self, ctx:CircuitSATParser.CircuitsatContext):
+        pass
+
+    # Exit a parse tree produced by CircuitSATParser#circuitsat.
+    def exitCircuitsat(self, ctx:CircuitSATParser.CircuitsatContext):
+        pass
+
+
+    # Enter a parse tree produced by CircuitSATParser#end.
+    def enterEnd(self, ctx:CircuitSATParser.EndContext):
+        pass
+
+    # Exit a parse tree produced by CircuitSATParser#end.
+    def exitEnd(self, ctx:CircuitSATParser.EndContext):
+        pass
+
+
+    # Enter a parse tree produced by CircuitSATParser#line.
+    def enterLine(self, ctx:CircuitSATParser.LineContext):
+        pass
+
+    # Exit a parse tree produced by CircuitSATParser#line.
+    def exitLine(self, ctx:CircuitSATParser.LineContext):
+        pass
+
+
+    # Enter a parse tree produced by CircuitSATParser#opcode.
+    def enterOpcode(self, ctx:CircuitSATParser.OpcodeContext):
+        pass
+
+    # Exit a parse tree produced by CircuitSATParser#opcode.
+    def exitOpcode(self, ctx:CircuitSATParser.OpcodeContext):
+        pass
+
+
+
+del CircuitSATParser
---
+++ circuitsat/CircuitSATVisitor.py
@@ -0,0 +1,33 @@
+# Generated from C:/Users/oscar/Downloads/Development/circuitsat\CircuitSAT.g4 by ANTLR 4.10.1
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .CircuitSATParser import CircuitSATParser
+else:
+    from CircuitSATParser import CircuitSATParser
+
+# This class defines a complete generic visitor for a parse tree produced by CircuitSATParser.
+
+class CircuitSATVisitor(ParseTreeVisitor):
+
+    # Visit a parse tree produced by CircuitSATParser#circuitsat.
+    def visitCircuitsat(self, ctx:CircuitSATParser.CircuitsatContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by CircuitSATParser#end.
+    def visitEnd(self, ctx:CircuitSATParser.EndContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by CircuitSATParser#line.
+    def visitLine(self, ctx:CircuitSATParser.LineContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by CircuitSATParser#opcode.
+    def visitOpcode(self, ctx:CircuitSATParser.OpcodeContext):
+        return self.visitChildren(ctx)
+
+
+
+del CircuitSATParser
